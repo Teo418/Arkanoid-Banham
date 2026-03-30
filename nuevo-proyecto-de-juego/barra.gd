e2eb17a504330ebb97e34ce43c48276d9c1bf0cf
Teo418 <teoibanham@gmail.com>
2026-03-30 17:38:55 -0300

    Tengo una buena base, pero la materia me la llevo igual me parece

--- nuevo-proyecto-de-juego/barra.gd
+++ nuevo-proyecto-de-juego/barra.gd
@@ -1,25 +1,16 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const VELOCIDAD_MOVIMIENTO = 300.0
+const PALETA = 25
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	var direccion := 0.0
+	if Input.is_action_pressed("ui_left"):
+		direccion = -1.5
+	if Input.is_action_pressed("ui_right"):
+		direccion = 1.5
+	velocity.x = direccion * VELOCIDAD_MOVIMIENTO
+	velocity.y = 0.0
 	move_and_slide()
+	var pantalla := get_viewport_rect().size
+	position.x = clamp(position.x, PALETA, pantalla.x - PALETA)
--- nuevo-proyecto-de-juego/pelota.gd
+++ nuevo-proyecto-de-juego/pelota.gd
@@ -10,9 +10,28 @@ func _ready() -> void:
 	velocity = Vector2(velocidad, -velocidad).normalized() * velocidad
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	var colision = move_and_collide(velocity * delta)
 	if colision:
 		var choque = colision.get_collider()
-		if choque.is_in_group("bloques")
+		if choque.is_in_group("bloque"):
 			choque.recibirChoque()
+			_aumentarVelocidad()
+			_verificarWin()
+		velocity = velocity.bounce(colision.get_normal())
+	var pantalla := get_viewport_rect().size
+	if position.x <= 0 or position.x >= pantalla.x:
+		velocity.x *= -1
+ 
+	if position.y <= 0:
+		velocity.y *= -1
+ 
+	if position.y >= pantalla.y:
+		get_tree().call_group("game_manager", "mostrar_game_over")
+
+func _aumentarVelocidad() -> void:
+	velocidad = min(velocidad+INCREMENTO_VELOCIDAD, VELOCIDAD_MAXIMA)
+	velocity = velocity.normalized() * velocidad
+func _verificarWin() -> void:
+	var bloquesRestantes = get_tree().get_nodes_in_group("bloques").size()
+	if bloquesRestantes == 0:
+		get_tree().call_group("game_manager", "mostrar_you_win")
